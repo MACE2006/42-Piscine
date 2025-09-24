bc7bf86ffc103b3efa475dfdb3710eb9e843e53e
Callum Vorley <cvorley@c3r4s2.42london.com>
2025-09-24 19:47:30 +0100

    Fixed 0 error

--- c05/ex03/ft_recursive_power.c
+++ c05/ex03/ft_recursive_power.c
@@ -6,11 +6,11 @@
 /*   By: cvorley <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/17 23:52:01 by cvorley           #+#    #+#             */
-/*   Updated: 2025/09/18 13:45:43 by cvorley          ###   ########.fr       */
+/*   Updated: 2025/09/24 19:44:49 by cvorley          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-//#include <stdio.h>
+#include <stdio.h>
 
 int	ft_recursive_power(int nb, int power)
 {
@@ -19,14 +19,14 @@ int	ft_recursive_power(int nb, int power)
 	r = nb;
 	if (power < 0)
 		return (0);
-	if (power == 0 && nb == 0)
+	if (power == 0)
 		return (1);
-	if (power > 1)
-		return (nb * ft_recursive_power(nb, (power - 1)));
+	else
+		return (nb * ft_recursive_power(nb, --power));
 	return (r);
 }
 
 /*int	main(void)
 {
-	printf("%d", ft_recursive_power(2, 2));
+	printf("%d", ft_recursive_power(0, 0));
 }*/
